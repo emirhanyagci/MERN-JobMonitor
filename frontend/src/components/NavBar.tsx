778ad6f52470e1b18907994dbc9eaedebc550350
Emirhan Yagci <emirhan.yac2@gmail.com>
2024-05-15 22:43:41 +0300

    sidebar sheet done

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,10 +1,40 @@
 import { Button } from "./ui/button";
+import { LogOut } from "lucide-react";
+import { Menu } from "lucide-react";
 
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 export default function NavBar() {
   return (
-    <nav className="w-full p-3 flex justify-end">
+    <nav className="w-full p-3 flex justify-between border-b md:justify-end">
+      <Sheet>
+        <SheetTrigger className="md:hidden">
+          <Menu />
+        </SheetTrigger>
+        <SheetContent side="left">
+          <SheetHeader>
+            <SheetTitle asChild>
+              <header className="tracking-widest text-2xl whitespace-nowrap">
+                Job Monitor
+              </header>
+            </SheetTitle>
+            <SheetDescription asChild>
+              <div className="flex flex-col gap-2 ">
+                <Button className="text-lg">Jobs</Button>
+                <Button className="text-lg ">Users</Button>
+              </div>
+            </SheetDescription>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
       <Button variant="ghost" className="text-md">
-        Logout
+        <LogOut />
       </Button>
     </nav>
   );
--- frontend/src/components/SideBar.tsx
+++ frontend/src/components/SideBar.tsx
@@ -2,13 +2,13 @@ import { Button } from "./ui/button";
 
 export default function SideBar() {
   return (
-    <aside className="h-screen p-8 space-y-5">
+    <aside className="h-screen p-8 space-y-5 border-r hidden md:block">
       <header className="tracking-widest text-2xl whitespace-nowrap">
         Job Monitor
       </header>
-      <div className="flex flex-col gap-2">
-        <Button variant="secondary">Jobs</Button>
-        <Button variant="secondary">Users</Button>
+      <div className="flex flex-col gap-2 ">
+        <Button className="text-base">Jobs</Button>
+        <Button className="text-base">Users</Button>
       </div>
     </aside>
   );
